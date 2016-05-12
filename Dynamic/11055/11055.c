a61ca03c8db2c7bce61faeff997914ef3ca640c7
jun <Sogang.jun.developer@gmail.com>
2016-05-12 20:02:05 +0900

    11722 가장긴감소하는수열 -unsolved

--- Dynamic/11055/11055.c
+++ Dynamic/11055/11055.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
 
+int a[1001];
+int d[1001];
+
 int main()
 {
+    int n,i,j;
+    long long ans=0;
+    scanf("%d",&n);
+    
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    d[0]=a[0];
+    for(i=1; i<n; i++)
+    {
+        d[i]=a[i];
+        for(j=0; j<i; j++)
+        {
+            if(a[i]>a[j] && d[i] < d[j]+a[i])
+            {
+                d[i]=d[j]+a[i];
+            }
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        if(ans<d[i])
+            ans=d[i];
+    }
+    printf("%lld\n",ans);
+    
     return 0;
 }
---
+++ Dynamic/11722/11722.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int a[1001];
+int d[1001];
+
+int main()
+{
+    int input;
+    int i,j;
+    int answer=0;
+    scanf("%d",&input);
+    for(i=0; i<input; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0; i<input; i++)
+    {
+        d[i]=1;
+        for(j=i-1; j>0; j--)
+        {
+            if(a[i]<a[j] && d[i]<=d[j])
+            {
+                d[i]=d[j]+1;
+            }
+        }
+    }
+    for(i=0; i<input;i++)
+    {
+        if(answer<d[i])
+            answer=d[i];
+    }
+    printf("%d\n",answer);
+    return 0;
+}
