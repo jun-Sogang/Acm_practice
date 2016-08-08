e557445a685e202b38ce0bdeae27a78fd8807361
jun <Sogang.jun.developer@gmail.com>
2016-08-08 18:23:07 +0900

    8/8

---
+++ august/ds/11656/11656.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cstring>
+char a[1001];
+using namespace std;
+
+int main()
+{
+    scanf("%s",a);
+    int len=a.lenth();
+    for(int i=0; i<len; i++)
+    {
+        q.push(a[i]);
+    }
+}
