4e740096ce27847ea919b8c2710597fa91398681
jun <Sogang.jun.developer@gmail.com>
2016-09-14 16:46:43 +0900

    9/14

--- august/dp2/11066/11066.cpp
+++ august/dp2/11066/11066.cpp
@@ -1,19 +1,34 @@
 #include <cstdio>
-
-int dp[505];
+#include <cstring>
 int a[505];
-void dynamic() {
+int preprocessing[505];
+int dp[505][505];
 
+int dynamic(int i,int j){
+    if(i == j)
+        return 0;
+    if(dp[i][j] != -1)
+        return dp[i][j];
+    int &ans = dp[i][j];
+    for(int k=i; k<=j-1; k++){
+        int temp = dynamic(i,k) + dynamic(k+1,j) + preprocessing[j]-preprocessing[i-1];
+        if(ans == -1 || ans > temp)
+            ans=temp;
+    }
+    return ans;
 }
-int main() {
-	int t;
-	scanf("%d", &t);
-	while (t--) {
-		int k;
-		scanf("%d", &k);
-		for (int i = 0; i < k; i++) {
-			scanf("%d", &a[k]);
-		}
-		dynamic();
-	}
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        memset(dp,-1,sizeof(dp));
+        memset(preprocessing,0,sizeof(preprocessing));
+        int n;
+        scanf("%d",&n);
+        for(int i=1; i<=n; i++){
+            scanf("%d",&a[i]);
+            preprocessing[i]=preprocessing[i-1]+a[i];
+        }
+        printf("%d\n",dynamic(1,n));
+    }
 }
