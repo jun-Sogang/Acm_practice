2a16e39f922e75f633459abc83d7ae850140018c
jun-Sogang <Sogang.jun.developer@gmail.com>
2016-11-09 16:44:12 +0900

    "11/9"

---
+++ august/kmp/1305/1305.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+vector < int > preprocessing(string a,int l){
+	vector < int > pi(l);
+	pi[0]=0;
+	int j=0;
+	for(int i=1; i<l; ++i){
+		while(j>0 && a[i] != a[j]){
+			j=pi[j-1];
+		}
+		if (a[i]==a[j]) {
+			pi[i]=j+1;
+			j++;
+		}
+		else{
+			pi[i]=0;
+		}
+	}
+	return pi;
+}
+int main(){
+	int l;
+	string a;
+	cin >> l >> a;
+	auto matched = preprocessing(a,l);
+	cout << l-matched[l-1];
+	puts("");
+}
--- august/kmp/1786/1786.cpp
+++ august/kmp/1786/1786.cpp
@@ -1,57 +1,57 @@
 #include <cstdio>
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <vector>
 using namespace std;
-vector<int> preprocessing(string p) {
-    int m = p.size();
-    vector<int> pi(m);
-    pi[0] = 0;
-    int j = 0;
-    for (int i=1; i<m; i++) {
-        while (j>0 && p[i] != p[j]) {
-            j = pi[j-1];
-        }
-        if (p[i] == p[j]) {
-            pi[i] = j+1;
-            j += 1;
-        } else {
-            pi[i] = 0;
-        }
-    }
-    return pi;
+
+vector < int > preprocessing(string p){
+	int string_size = p.size();
+	vector < int > pi(string_size);
+	pi[0]=0;
+	int j=0;
+	for(int i=1; i<string_size; i++){
+		while (j>0 && p[i] != p[j]) {
+			j=pi[j-1];
+		}
+		if(p[i]==p[j]){
+			pi[i] = j+1;
+			j++;
+		}
+		else{
+			pi[i]=0;
+		}
+	}
+	return pi;
 }
-vector<int> kmp(string s, string p) {
-    auto pi = preprocessing(p);
-    vector<int> ans;
-    int n = s.size();
-    int m = p.size();
-    int i = 0;
-    int j = 0;
-    for(int i=0; i<n; i++) {
-        while (j > 0 && s[i] != p[j]) {
-            j = pi[j-1];
-        }
-        if (s[i] == p[j]) {
-            if (j == m-1) {
-                ans.push_back(i-m+1);
-                j = pi[j];
-            } else {
-                j += 1;
-            }
-        }
-    }
-    return ans;
+vector < int > kmp(string t, string p){
+	auto pi = preprocessing(p);
+	vector < int > ans;
+	int n=t.size();
+	int m=p.size();
+	int j=0;
+	for(int i=0; i<n; i++){
+		while(j>0 && t[i] != p[j]){
+			j=pi[j-1];
+		}
+		if(t[i] == p[j]){
+			if(j==m-1){
+				ans.push_back(i-m+1);
+				j=pi[j];
+			}
+			else{
+				j++;
+			}
+		}
+	}
+	return ans;
 }
-int main() {
-    string s,p;
-    getline(cin,s);
-    getline(cin,p);
-    auto matched = kmp(s, p);
-    cout << matched.size() << '\n';
-    for (auto index : matched) {
-        cout << index+1 << ' ';
-    }
-    cout << '\n';
-    return 0;
+int main(){
+	string t,p;
+	getline(cin,t);
+	getline(cin,p);
+	auto matched = kmp(t,p);
+	cout << matched.size() << '\n';
+	for(auto index : matched){
+		cout << index+1 << ' ';
+	}
 }
