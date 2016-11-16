96ce9e6eaa5e900a6347d6e191ad66375febfebf
jun-Sogang <Sogang.jun.developer@gmail.com>
2016-11-16 12:54:46 +0900

    "고쳐야함"

--- august/graph2/11403/11403.cpp
+++ august/graph2/11403/11403.cpp
@@ -7,7 +7,8 @@ void floyd_warshall() {
 		for (int j = 1; j <= n; j++)
 			for (int k = 1; k <= n; k++)
 				if (a[j][i] == 1 && a[i][k] == 1)
-					a[j][k] = a[j][i]+
+					a[j][k] = a[j][i]+;
+					//고쳐야함
 }
 int main() {
 	scanf("%d", &n);
